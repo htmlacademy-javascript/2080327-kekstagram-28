10472a3b5781ccbf451f9da8d0ded02e6ffd7f47
Анна <a.sergeeva.project@gmail.com>
2023-03-07 20:42:33 +0300

    Решает задачи (делает микро рефакторинг)

--- js/function.js
+++ js/function.js
@@ -1,17 +1,11 @@
-function comparesLength (parametrString,maxLength) {
-  if (parametrString.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
-}
+const comparesLength = (parametrString,maxLength) => parametrString.length <= maxLength
 comparesLength('проверяемая строка', 20);
 
-function palindromeChecker(str) {
-  str = str.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
-  const lastIndex = str.length - 1;
-  for (let i = 0; i < str.length / 2; i++) {
-    if (str[i] !== str[lastIndex - i]) {
+const palindromeChecker = (str) => {
+  const text = str.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
+  const lastIndex = text.length - 1;
+  for (let i = 0; i < text.length / 2; i++) {
+    if (text[i] !== text[lastIndex - i]) {
       return false;
     }
   }
@@ -19,21 +13,20 @@ function palindromeChecker(str) {
 }
 palindromeChecker('bkbkjbkjbk');
 
-function numberChecker(enterString) {
-  const str = `${ enterString}`;
+const exportNumber = (enterString) => {
+  const str = `${ enterString }`;
   const res = str.replace(/\D/g, '');
   return res;
 }
-numberChecker('34');
+exportNumber('34');
 
-function fileName(enterString,minLength,addSymbol) {
+const padStart = (enterString, minLength, addSymbol) => {
   while (minLength > enterString.length) {
     if(minLength - enterString.length < addSymbol.length) {
       addSymbol = addSymbol.substring(0, minLength - enterString.length);
-
     }
-    enterString = `${ addSymbol }${enterString}`;
+    enterString = `${ addSymbol }${ enterString }`;
   }
   return enterString;
 }
-fileName('dgdhfhf');
+padStart('dgdhfhf');

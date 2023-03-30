5fd7c120d515abbbd89c21608000680b48036bd3
Анна <a.sergeeva.project@gmail.com>
2023-03-30 21:29:26 +0300

    Пишет генерацию данных - поправляет ошибки линтера

--- js/main.js
+++ js/main.js
@@ -1,41 +1,4 @@
-
-const getLikes = (first,last) => {
-  const lower = Math.ceil(Math.min(first, last));
-  const upper = Math.floor(Math.max(first, last));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-const descriptionsFirst = [
-  'описание1',
-  'описание2',
-  'описание3',
-  'описание4',
-  'описание5',
-];
-
-const descriptionsSecond = [
-  'дополнение1',
-  'дополнение2',
-  'дополнение3',
-  'дополнение4',
-];
-const getDescription = () => {
-  let lower = Math.ceil(Math.min(0, descriptionsFirst.length - 1));
-  let upper = Math.floor(Math.max(0, descriptionsFirst.length - 1));
-  let result = Math.random() * (upper - lower + 1) + lower;
-  const keyFirst = Math.floor(result);
-
-  lower = Math.ceil(Math.min(0, descriptionsSecond.length - 1));
-  upper = Math.floor(Math.max(0, descriptionsSecond.length - 1));
-  result = Math.random() * (upper - lower + 1) + lower;
-  const keySecond = Math.floor(result);
-
-  return descriptionsFirst[keyFirst] + descriptionsSecond[keySecond];
-};
-function generateRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-const textComment = [
+const TEXT_COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -43,15 +6,8 @@ const textComment = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-const getTextComment = () => {
-  const lower = Math.ceil(Math.min(0, textComment.length - 1));
-  const upper = Math.floor(Math.max(0, textComment.length - 1));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  const keyFirst = Math.floor(result);
 
-  return textComment[keyFirst];
-};
-const firstName = [
+const FIRST_NAME = [
   'Вася',
   'Дима',
   'Леша',
@@ -59,39 +15,68 @@ const firstName = [
   'Влад',
   'Паша'
 ];
-const getName = () => {
-  const lower = Math.ceil(Math.min(0, firstName.length - 1));
-  const upper = Math.floor(Math.max(0, firstName.length - 1));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  const keyFirst = Math.floor(result);
 
-  return firstName[keyFirst];
+const DESCRIPTION_FIRST = [
+  'описание1',
+  'описание2',
+  'описание3',
+  'описание4',
+  'описание5',
+];
+
+const DESCRIPTION_SECOND = [
+  'дополнение1',
+  'дополнение2',
+  'дополнение3',
+  'дополнение4',
+];
+const COMMENTS_COUNT = 2;
+const AVATAR_MIN_NUMBER = 1;
+const AVATAR_MAX_NUMBER = 6;
+const PHOTOS_COUNT = 25;
+const MIN_LIKE_COUNT = 15;
+const MAX_LIKE_COUNT = 200;
+const generateRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getDescription = () => {
+  const keyFirst = generateRandomInteger(0,DESCRIPTION_FIRST.length - 1);
+  const keySecond = generateRandomInteger(0,DESCRIPTION_SECOND.length - 1);
+
+  return DESCRIPTION_FIRST[keyFirst] + DESCRIPTION_SECOND[keySecond];
 };
 
-const getData = function() {
+const getTextComment = () => {
+  const keyFirst = generateRandomInteger(0,TEXT_COMMENTS.length - 1);
+  return TEXT_COMMENTS[keyFirst];
+};
 
-  const data = [];
+const getName = () => {
+  const keyFirst = generateRandomInteger(0,FIRST_NAME.length - 1);
+  return FIRST_NAME[keyFirst];
+};
+const getComments = () => {
   const comments = [];
-
-  for (let j = 0; j < 2; j++) {
+  for (let j = 0; j < COMMENTS_COUNT; j++) {
     comments.push({
       id: j + 1,
-      avatar: 'img/avatar-' + generateRandomInteger(1,6) + '.svg',
+      avatar: `img/avatar-${generateRandomInteger(AVATAR_MIN_NUMBER,AVATAR_MAX_NUMBER)}.svg`,
       message: getTextComment(),
       name: getName()
     });
   }
+  return comments;
+};
+const getData = () => {
 
-  for (let i = 0; i < 25; i++) {
+  const data = [];
+
+  for (let i = 0; i < PHOTOS_COUNT; i++) {
     data.push({
       id: i + 1,
-      url:'photos/' + (i + 1) + '.jpg',
-      likes: getLikes(15,200),
+      url:`photos/${(i + 1)}.jpg`,
+      likes: generateRandomInteger(MIN_LIKE_COUNT,MAX_LIKE_COUNT),
       description: getDescription(),
-
+      comments: getComments()
     });
-
-    data[i].comments = comments;
   }
   return data;
 };
